8929f0221c8c0e2c297eff1e997f195adc78aacb
wac <wabaham9@gmail.com>
2023-12-01 00:38:36 +0300

    Add render scripts

--- config/puma.rb
+++ config/puma.rb
@@ -21,15 +21,16 @@ end
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
 # terminating a worker in development environments.
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
-
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 port ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 environment ENV.fetch("RAILS_ENV") { "development" }
 
+workers ENV.fetch("WEB_CONCURRENCY") { 4 }
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
+preload_app!
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
