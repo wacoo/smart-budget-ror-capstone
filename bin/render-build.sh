bfb7cd2547839e70f77ce6bc0e9237353fef8d31
wac <wabaham9@gmail.com>
2023-12-01 01:28:23 +0300

    Drop and create database to clean

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,6 +5,6 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-# rails db:drop DISABLE_DATABASE_ENVIRONMENT_CHECK=1
-# rails db:create
+rails db:drop DISABLE_DATABASE_ENVIRONMENT_CHECK=1
+rails db:create
 bundle exec rake db:migrate
